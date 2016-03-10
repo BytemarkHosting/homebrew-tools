b79380df41f63a8c891a7944b6ee651d51649935
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-03-10 14:55:39 +0000

    Update bytemark-client to 0.5.0

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -1,8 +1,8 @@
 class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
   homepage "https://bigv.io"
-  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.4.268.tar.gz"
-  sha256 "3d984e6875a67c140c624e0684e5c342aa1011e66f1b34785f11ce3df638ae21"
+  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.5.0.tar.gz"
+  sha256 "e37de8c3a8d4cf50d7a3326ff0c93cd8aa39ddf877dca924070060d1806bea7e"
 
   bottle :unneeded
 
