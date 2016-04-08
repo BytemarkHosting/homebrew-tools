466ac66d256690ffbd5529798849ed77acfff4f8
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-04-08 15:33:12 +0100

    Update homepage and bump version

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -1,8 +1,8 @@
 class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
-  homepage "https://bigv.io"
-  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.5.0.tar.gz"
-  sha256 "e37de8c3a8d4cf50d7a3326ff0c93cd8aa39ddf877dca924070060d1806bea7e"
+  homepage "https://bytemark.co.uk/docs/command-line"
+  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.6.0.tar.gz"
+  sha256 "5adffe5b2189742625745086fce8f071ac8b99e038d633ea753f2c1fb0f07974"
 
   bottle :unneeded
 
