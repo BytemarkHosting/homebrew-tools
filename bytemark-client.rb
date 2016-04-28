1b847d7de9d0f0402cf51fd38ad9b1b02c788f78
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-04-28 16:02:18 +0100

    Release 0.7.0

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -1,13 +1,15 @@
 class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
   homepage "https://bytemark.co.uk/docs/command-line"
-  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.6.0.tar.gz"
-  sha256 "5adffe5b2189742625745086fce8f071ac8b99e038d633ea753f2c1fb0f07974"
+  url "http://repo.bytemark.co.uk/bytemark-client/osx/bytemark-0.7.0.tar.gz"
+  sha256 "39b27f5808b417cd3fe86992fe1a7c44a1375ad5a3143312f15e40e2440b7663"
 
   bottle :unneeded
 
   def install
     bin.install "bytemark"
+    #man1.mkpath
+    #man1.install "doc/bytemark.1"
   end
 
   test do
