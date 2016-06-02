4c3eca0b3c10791eea4faaf0731988b8c4dc037c
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-06-02 14:47:05 +0100

    bytemark-client: Update sha256 hash to match the current one on repo

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -2,7 +2,7 @@ class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
   homepage "https://bytemark.co.uk/docs/command-line"
   url "http://repo.bytemark.co.uk/bytemark-client/mac/bytemark-0.7.1.tar.gz"
-  sha256 "b98ff806b80ab037f85203acbb532cdd5f7772e4e463f2c78a9c68ad0bc14dd4"
+  sha256 "f6a366a04d8f0b9a931acaaa84f658d4fd5cdc4223934aec82fde6987e138f10"
 
   bottle :unneeded
   
