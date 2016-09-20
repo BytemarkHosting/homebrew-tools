444ca310ac2c68cb40245616f6e606c5bfd27ddd
telyn <telyn@bytemark.co.uk>
2016-09-20 17:06:01 +0100

    bump bytemark-client to 1.1

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -1,8 +1,8 @@
 class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
   homepage "https://bytemark.co.uk/docs/command-line"
-  url "http://repo.bytemark.co.uk/bytemark-client/mac/bytemark-1.0.tar.gz"
-  sha256 "bcb6130eba6bcf24d6b677bd61d0261622253e3560d480a63c97b9cac937778a"
+  url "http://repo.bytemark.co.uk/bytemark-client/mac/bytemark-1.1.tar.gz"
+  sha256 "62a6d1bac33fdb69b270ca1b1fa9cfd16ab83e270c3626663714df5624d5306d"
 
   bottle :unneeded
   
