48308c07da1ce43d75cc58866370de29955c4c75
telyn <telyn@bytemark.co.uk>
2017-03-07 11:19:35 +0000

    Add beta version of client (current just alias - wait for the next push by Bytemark Automation)

---
+++ bytemark-client-beta.rb
@@ -0,0 +1,18 @@
+class BytemarkClientBeta < Formula
+  desc "in-development version of the command-line client for Bytemark services"
+  homepage "https://bytemark.co.uk/docs/command-line"
+  url "https://repo.bytemark.co.uk/bytemark-client/mac/bytemark-2.1.tar.gz"
+  sha256 "78fc1ed8f8e501f532a2a844dc73955a0ff731840b995f5d126f40a3f28f9d50"
+
+  bottle :unneeded
+  
+  def install
+    bin.install "bytemark"
+    man1.mkpath
+    man1.install "doc/bytemark.1"
+  end
+
+  test do
+    system "bin/bytemark", "version"
+  end
+end
