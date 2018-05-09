05b5cde636b7309eb5efcad1d934e826bf89d950
Bytemark <nobody@bytemark.co.uk>
2018-05-09 12:36:51 +0000

    Update bytemark-client to version VERSION

--- bytemark-client.rb
+++ bytemark-client.rb
@@ -2,7 +2,7 @@ class BytemarkClient < Formula
   desc "command-line client for Bytemark services"
   homepage "https://bytemark.co.uk/docs/command-line"
   url "https://repo.bytemark.co.uk/bytemark-client/mac/bytemark-3.1.tar.gz"
-  sha256 "67edbfd8fcb9415eff85c7ceefa91d78a7a0af1b5a0d7a01c251a791bf49c764"
+  sha256 "c88a994c52a9d08ba248054740eac38a808f89185bfffbe01b6f0ad6ee0d5777"
 
   bottle :unneeded
   
