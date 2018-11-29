71902e680f8590ea9767d00c074e1394a26b2c9f
Bytemark <nobody@bytemark.co.uk>
2018-11-29 11:25:34 +0000

    Update bytemark-client-beta to version VERSION

--- bytemark-client-beta.rb
+++ bytemark-client-beta.rb
@@ -1,8 +1,8 @@
 class BytemarkClientBeta < Formula
   desc "in-development version of the command-line client for Bytemark services"
   homepage "https://bytemark.co.uk/docs/command-line"
-  url "https://repo.bytemark.co.uk/open-source/bytemark-client/develop/74970/mac/bytemark-3.4~74970.git295c52b.tar.gz"
-  sha256 "ce19b4e676f78d0bd6dc828f3f6c6d8c8233cc1abbcc0cd8932381c3f2d3b1e1"
+  url "https://repo.bytemark.co.uk/open-source/bytemark-client/develop/78419/mac/bytemark-3.4~78419.gitb07c26a.tar.gz"
+  sha256 "f3d5e155638bd98d8b427d1e464a7a0da01ae7f475012be4513324f9f7a6dd68"
 
   bottle :unneeded
   
